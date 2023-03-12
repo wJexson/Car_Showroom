75e6d2b5719f3b85979b270f2080fa32f454fd5b
Daniil <danilmega2003@gmail.com>
2023-03-12 21:37:46 +0300

    added Log and Toast system to ListView list

--- app/src/main/java/com/example/carshowroom/ListViewFragment.java
+++ app/src/main/java/com/example/carshowroom/ListViewFragment.java
@@ -1,12 +1,15 @@
 package com.example.carshowroom;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -14,6 +17,9 @@ import androidx.fragment.app.FragmentTransaction;
 import java.util.ArrayList;
 
 public class ListViewFragment extends Fragment {
+
+    private static final String TAG = "ListView";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_listview, container, false);
@@ -27,6 +33,16 @@ public class ListViewFragment extends Fragment {
         ListView brandsList = view.findViewById(R.id.brandsList);
         BrandAdapter brandAdapter = new BrandAdapter(getContext(), R.layout.list_item, brands);
         brandsList.setAdapter(brandAdapter);
+
+        brandsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // по позиции получаем выбранный элемент
+                String selectedItem = all_brands[position];
+                Log.i(TAG, "You clicked on " + selectedItem);
+                Toast.makeText(getActivity(),selectedItem,Toast.LENGTH_SHORT).show();
+            }
+        });
 
         back_button.setOnClickListener(new View.OnClickListener() {
             @Override
