e6ac8a1045887c0390fbebef30998bee720ac8fe
Daniil <danilmega2003@gmail.com>
2023-03-13 18:28:18 +0300

    started practice 5

--- app/src/main/java/com/example/carshowroom/MainFragment.java
+++ app/src/main/java/com/example/carshowroom/MainFragment.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 
 public class MainFragment extends Fragment {
 
@@ -17,48 +17,59 @@ public class MainFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         Button skyline_button = view.findViewById(R.id.skyline_button);
         Button rs6_button = view.findViewById(R.id.rs6_button);
+        Button recyclerview_button = view.findViewById(R.id.recyclerview_button);
         Button skyline_bet_button = view.findViewById(R.id.skyline_bet_button);
         Button rs6_bet_button = view.findViewById(R.id.rs6_bet_button);
 
 
         skyline_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                FragmentTransaction fr = getFragmentManager().beginTransaction();
-                fr.replace(R.id.fragment_container_view, new SkylineFragment());
-                fr.commit();
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_skylineFragment);
             }
         });
 
         rs6_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                FragmentTransaction fr = getFragmentManager().beginTransaction();
-                fr.replace(R.id.fragment_container_view, new RS6Fragment());
-                fr.commit();
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_rs6Fragment);
             }
         });
 
-        Button listview_button = view.findViewById(R.id.listview_button);
-        Button recycleview_button = view.findViewById(R.id.recycleview_button);
-
-        listview_button.setOnClickListener(new View.OnClickListener() {
+        recyclerview_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                FragmentTransaction fr = getFragmentManager().beginTransaction();
-                fr.replace(R.id.fragment_container_view, new ListViewFragment());
-                fr.commit();
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_brandslistFragment);
             }
         });
 
-        recycleview_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction fr = getFragmentManager().beginTransaction();
-                fr.replace(R.id.fragment_container_view, new RecyclerViewFragment());
-                fr.commit();
-            }
-        });
+//        skyline_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FragmentTransaction fr = getFragmentManager().beginTransaction();
+//                fr.replace(R.id.fragment_container_view, new SkylineFragment());
+//                fr.commit();
+//            }
+//        });
+//
+//        rs6_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FragmentTransaction fr = getFragmentManager().beginTransaction();
+//                fr.replace(R.id.fragment_container_view, new RS6Fragment());
+//                fr.commit();
+//            }
+//        });
+//
+//
+//        recyclerview_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FragmentTransaction fr = getFragmentManager().beginTransaction();
+//                fr.replace(R.id.fragment_container_view, new RecyclerViewFragment());
+//                fr.commit();
+//            }
+//        });
 
         skyline_bet_button.setOnClickListener(new View.OnClickListener() {
             @Override
