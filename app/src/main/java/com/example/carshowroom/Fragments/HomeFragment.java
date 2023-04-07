435e8733d3a81d8786e1aebce6c551c7198a2fec
Daniil <danilmega2003@gmail.com>
2023-04-07 11:25:25 +0300

    updated fragment_home.xml

--- app/src/main/java/com/example/carshowroom/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/carshowroom/Fragments/HomeFragment.java
@@ -39,8 +39,6 @@ public class HomeFragment extends Fragment {
         Button skyline_button = view.findViewById(R.id.skyline_button);
         Button rs6_button = view.findViewById(R.id.rs6_button);
         Button recyclerview_button = view.findViewById(R.id.recyclerview_button);
-        Button skyline_bet_button = view.findViewById(R.id.skyline_bet_button);
-        Button rs6_bet_button = view.findViewById(R.id.rs6_bet_button);
         Button notification_button = view.findViewById(R.id.notification_button);
         createNotificationChannel();
 
@@ -63,34 +61,6 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_brandslistFragment);
-            }
-        });
-
-        skyline_bet_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText skyline_et = view.findViewById(R.id.skyline_bet_edittext);
-                String bet_data = skyline_et.getText().toString();
-                if (bet_data.equals("")) {
-                    bet_data = "0";
-                }
-                Bundle bet = new Bundle();
-                bet.putString("skyline_bet_Key", bet_data);
-                Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_skylineFragment, bet);
-            }
-        });
-
-        rs6_bet_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText rs6_et = view.findViewById(R.id.rs6_bet_edtttext);
-                String bet_data = rs6_et.getText().toString();
-                if (bet_data.equals("")) {
-                    bet_data = "0";
-                }
-                Bundle bet = new Bundle();
-                bet.putString("rs6_bet_Key", bet_data);
-                Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_rs6Fragment, bet);
             }
         });
 
