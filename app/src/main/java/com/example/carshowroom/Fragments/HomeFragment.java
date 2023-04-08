a2bdb985da63af4cc78c0282c8c2e305cef7b042
Daniil <danilmega2003@gmail.com>
2023-04-08 09:54:00 +0300

    few updates in markup

--- app/src/main/java/com/example/carshowroom/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/carshowroom/Fragments/HomeFragment.java
@@ -1,12 +1,5 @@
 package com.example.carshowroom.Fragments;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,15 +8,12 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import com.example.carshowroom.R;
 
 public class HomeFragment extends Fragment {
-
-    private final String CHANNEL_ID = "notification";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -36,9 +26,6 @@ public class HomeFragment extends Fragment {
         Button skyline_button = view.findViewById(R.id.skyline_button);
         Button rs6_button = view.findViewById(R.id.rs6_button);
         Button recyclerview_button = view.findViewById(R.id.recyclerview_button);
-        Button notification_button = view.findViewById(R.id.notification_button);
-        createNotificationChannel();
-
 
         skyline_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,39 +47,5 @@ public class HomeFragment extends Fragment {
                 Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_brandslistFragment);
             }
         });
-
-        notification_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showNotification();
-//                Intent intent = new Intent(requireContext(), CarService.class);
-//                requireActivity().startService(intent);
-            }
-        });
-    }
-
-    private void showNotification() {
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.logo, options);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(requireContext(), CHANNEL_ID)
-                .setAutoCancel(true)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentText("Получено новое уведомление")
-                .setContentTitle(getString(R.string.app_name))
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setLargeIcon(bitmap)
-                .setWhen(System.currentTimeMillis());
-
-        Notification notification = notificationBuilder.build();
-        NotificationManager notificationManager = (NotificationManager) requireContext().getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(1, notification);
-    }
-
-    private void createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_ID, NotificationManager.IMPORTANCE_DEFAULT);
-            NotificationManager notificationManager = (NotificationManager) requireContext().getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.createNotificationChannel(channel);
-        }
     }
 }
