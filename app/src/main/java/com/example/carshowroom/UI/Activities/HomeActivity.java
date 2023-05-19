7ae81c4ce78509d8af43e77cbc3a0b87fa3e66a2
Daniil <danilmega2003@gmail.com>
2023-05-19 17:14:35 +0300

    сделан bottom navigation

--- app/src/main/java/com/example/carshowroom/UI/Activities/HomeActivity.java
+++ app/src/main/java/com/example/carshowroom/UI/Activities/HomeActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.carshowroom.R;
--- app/src/main/java/com/example/carshowroom/UI/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/carshowroom/UI/Fragments/HomeFragment.java
@@ -31,8 +31,6 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Button brandList_button = view.findViewById(R.id.brandList_button);
-        Button review_button = view.findViewById(R.id.review_button);
 
         CarAdListViewModel carAdListViewModel = new ViewModelProvider(this).get(CarAdListViewModel.class);
         carAdListViewModel.carAdList.observe(getViewLifecycleOwner(), new Observer<List<CarAd>>() {
@@ -50,20 +48,6 @@ public class HomeFragment extends Fragment {
                 };
                 RecyclerView carAdList = view.findViewById(R.id.carAdList);
                 carAdList.setAdapter(carAdListAdapter);
-            }
-        });
-
-        brandList_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_brandslistFragment);
-            }
-        });
-
-        review_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_reviewFragment);
             }
         });
     }
