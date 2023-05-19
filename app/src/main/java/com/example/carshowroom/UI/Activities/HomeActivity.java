244cb6b4fe2fe76c6a591b9ae9aff11f2ba647b5
Daniil <danilmega2003@gmail.com>
2023-05-19 14:55:23 +0300

    продолжается работа над bottom navigation

--- app/src/main/java/com/example/carshowroom/UI/Activities/HomeActivity.java
+++ app/src/main/java/com/example/carshowroom/UI/Activities/HomeActivity.java
@@ -3,13 +3,27 @@ package com.example.carshowroom.UI.Activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.carshowroom.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
